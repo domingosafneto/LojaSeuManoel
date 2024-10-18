5960bfaa2265f491300b4570d757311d7dac1bab
Domingos Neto <domingosafneto@gmail.com>
2024-10-18 16:46:52 -0300

    scripts do banco

---
+++ Banco/scriptDDL.sql
@@ -0,0 +1,90 @@
+-- Criação das tabelas
+use DB_LojaSeuManoel; 
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- tabela Produto
+IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS 
+           WHERE CONSTRAINT_NAME = 'FK_PedidoProduto_Produto')
+BEGIN
+	ALTER TABLE PedidoProduto DROP CONSTRAINT FK_PedidoProduto_Produto
+	PRINT 'A constraint FK_PedidoProduto_Produto foi removida.'
+END
+GO
+
+IF OBJECT_ID('Produto', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE Produto
+	PRINT 'A tabela Produto foi removida.'	
+END
+GO
+
+CREATE TABLE Produto (
+	IdProduto INT IDENTITY(1,1) PRIMARY KEY,
+	Nome VARCHAR(50) NOT NULL,
+	Preco DECIMAL(10,2) NULL,
+	Altura DECIMAL(5,2) NULL,
+	Largura DECIMAL(5,2) NULL,
+	Comprimento DECIMAL(5,2) NULL
+);
+GO
+
+PRINT 'A tabela Produto foi criada.'
+GO
+
+
+-- tabela Pedido
+IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS 
+           WHERE CONSTRAINT_NAME = 'FK_PedidoProduto_Pedido')
+BEGIN
+	ALTER TABLE PedidoProduto DROP CONSTRAINT FK_PedidoProduto_Pedido
+	PRINT 'A constraint FK_PedidoProduto_Pedido foi removida.'
+END
+GO
+
+IF OBJECT_ID('Pedido', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE Pedido
+	PRINT 'A tabela Pedido foi removida.'	
+END
+GO
+
+CREATE TABLE Pedido (
+	IdPedido INT IDENTITY(1,1) PRIMARY KEY,
+	Descricao VARCHAR(70) NOT NULL,
+	Preco DECIMAL(10,2) NULL,
+	Altura DECIMAL(5,2) NULL,
+	Largura DECIMAL(5,2) NULL,
+	Comprimento DECIMAL(5,2) NULL
+);
+GO
+
+PRINT 'A tabela Pedido foi criada.'
+GO
+
+
+-- tabela PedidoProduto 
+IF OBJECT_ID('PedidoProduto', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE PedidoProduto
+	PRINT 'A tabela PedidoProduto foi removida.'	
+END
+GO
+
+CREATE TABLE PedidoProduto (
+	IdPedido INT NOT NULL,
+	IdProduto INT NOT NULL,	
+	Quantidade INT NOT NULL DEFAULT 1,
+	CONSTRAINT PK_PedidoProduto PRIMARY KEY (IdProduto, IdPedido),
+	CONSTRAINT FK_PedidoProduto_Pedido FOREIGN KEY (IdPedido) REFERENCES Pedido(IdPedido),
+	CONSTRAINT FK_PedidoProduto_Produto FOREIGN KEY (IdProduto) REFERENCES Produto(IdProduto)	
+);
+GO
+
+PRINT 'A tabela PedidoProduto foi criada.'
+GO
---
+++ Banco/scriptDML.sql
@@ -0,0 +1,16 @@
+USE DB_LojaSeuManoel
+GO
+
+-- script de carga 
+
+-- cadastro de produto
+INSERT INTO Produto (Nome, Preco, Altura, Largura, Comprimento) VALUES ('Produto A', 150.75, 75.00, 45.00, 35.00);
+
+INSERT INTO Produto (Nome, Preco, Altura, Largura, Comprimento) VALUES ('Produto B', 200.00, 60.50, 40.00, 30.00);
+
+INSERT INTO Produto (Nome, Preco, Altura, Largura, Comprimento) VALUES ('Produto C', 320.99, 120.00, 55.00, 50.00);
+
+INSERT INTO Produto (Nome, Preco, Altura, Largura, Comprimento) VALUES ('Produto D', 450.25, 90.00, 60.00, 45.00);
+
+INSERT INTO Produto (Nome, Preco, Altura, Largura, Comprimento) VALUES ('Produto E', 600.50, 110.00, 70.00, 65.00);
+GO
